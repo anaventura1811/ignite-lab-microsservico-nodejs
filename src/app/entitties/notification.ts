65923daf0fe3a2581fed3911d58aff866aef5624
Ana Ventura <anaventura1811@gmail.com>
2022-12-13 19:54:35 -0300

    feat: cria nova estrutura de pastas para a aplicação, estrutura o app com base na identificação de entidades, cria classes para as entidades, cria arquivo de config do jest

---
+++ src/app/entitties/notification.ts
@@ -0,0 +1,53 @@
+import { Content } from './content';
+
+interface NotificationProps {
+  recipientId: string;
+  content: Content;
+  category: string;
+  readtAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set content(content: Content) {
+    this.props.content = content;
+  }
+
+  public get content(): Content {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set recipientId(id: string) {
+    this.props.recipientId = id;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readtAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readtAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
